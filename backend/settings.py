45091251970580f81cd854785095c7d14d64bd93
bihari-bhau <sdr95958@gmail.com>
2026-03-21 21:44:28 +0530

    Fix email config with defaults

--- backend/settings.py
+++ backend/settings.py
@@ -160,3 +160,11 @@ SWAGGER_SETTINGS = {
         }
     }
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST', default='smtpout.secureserver.net')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='admin@biharskillhub.co.in')
